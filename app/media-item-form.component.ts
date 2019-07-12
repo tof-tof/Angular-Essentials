1e1a5c58a222ebf44ef0588ff86f555cbc4a5598
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2019-07-12 20:46:31 +0100

    custom validator for year added

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -16,11 +16,25 @@ export class MediaItemFormComponent {
       medium: new FormControl('Movies'),
       name: new FormControl('', Validators.compose([
         Validators.required,
-        Validators.pattern('[\\w\\-\\s\\/]+'])),
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
       category: new FormControl(''),
-      year: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
     });
+  }
 
+  yearValidator(control) {
+    if(control.value.trim().length ===0){
+      return null;
+    }
+    let year = parseInt(control.value);
+    let minYear = 1900;
+    let maxYear = 2100;
+    if(year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return {'year': true};
+    }
   }
   onSubmit(mediaItem) {
     console.log(mediaItem);
