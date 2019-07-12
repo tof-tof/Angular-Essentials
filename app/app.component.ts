1eba340caa0a9b60be223473363a20e90cbce702
Tof Adigun-Hameed <tofunmi.ah@gmail.com>
2019-07-12 10:14:43 +0100

    end of section 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['app/app.component.css']
 })
 export class AppComponent {
+  onMediaItemDelete(mediaItem){
+    
+  }
   firstMediaItem = {
     id: 1,
     name: "Firebug",
