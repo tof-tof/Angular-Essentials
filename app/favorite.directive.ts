0ec82aa71d7045ed5fe846e0f30397c44066cc1a
Tof Adigun-Hameed <tofunmi.ah@gmail.com>
2019-07-12 10:48:40 +0100

    adding isfavourite directive

---
+++ app/favorite.directive.ts
@@ -0,0 +1,13 @@
+import {Directive, HostBinding} from '@angular/core';
+
+@Directive({
+  selector: '[mwFavorite]',
+
+})
+
+
+
+
+export class FavoriteDirective {
+  @HostBinding('class.is-favorite') isFavorite = true;
+}
